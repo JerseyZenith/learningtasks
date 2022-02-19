f7c291ca8c5146429bc7d8482925c02e3f9d33ae
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-02-20 00:35:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 	<title>My Home Page</title>
 	<head><h1>Welcome To My Website! My Name is Jersey Zenith V. Setera</h1></head>
   <body>
-	  <ul>
+  <ul>
       <li><a href="index.php">HOME</a></li>
-      <li><a href="Recipes.html"><li>Learning Task 2</li></a></li>
+      <li><a href="Recipes.html">Learning Task 2</a></li>
     </ul>
   </body>
 	
