72ed3f937d0bbbb33c474bcb5434b727e88caff3
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-02-20 00:40:19 +0800

    Create Recipes.php

---
+++ LT2/Recipes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<title>Recipes</title>
+	<head><h1>Recipes</h1></head>
+		<ul>
+		<body>
+		<a href="ChiliConCarne.html"><li>Chili Con Carne</li></a>
+		<a href="BeefInBeer.html"><li>Beef in Beer</li></a>
+		<a href="PadThaiChicken.html"><li>Pad Thai Chicken</li></a>
+		<a href="ThaiGreenCurry.html"><li>Thai Green Curry</li></a>
+		<a href="PineappleChicken.html"><li>Pineapple Chicken</li></a>
+		<a href="VegetarianRice.html"><li>Vegetarian Rice</li></a>
+		<a href="Omelette.html"><li>Omelette</li></a>
+		</ul>
+		</body>
+	
+	
+</html>
