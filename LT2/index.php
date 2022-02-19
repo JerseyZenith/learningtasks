7469851b546e5298740d76ee1bb43c62fa1681b2
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-02-20 00:42:13 +0800

    Create index.php

---
+++ LT2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<title>My Home Page</title>
+	<head><h1>Welcome To My Website! My Name is Jersey Zenith V. Setera</h1></head>
+  <body>
+  <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="Recipes.php">Learning Task 2</a></li>
+    </ul>
+  </body>
+	
+</html>
