113502721e2ddf053bdf6ec825b47ec0510899cb
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-03-14 00:27:10 +0800

    Create index.php

---
+++ LT3/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Top 10 Favorite Songs</title>
+		<h1>My Top 10 Favorite Songs</h1>
+		 <link rel="stylesheet" type="text/css" href="Style.css"/>
+	</head>
+	<body>
+		<div id="col">
+		     <div>Top 1 Change by J.Cole<center><img src="change.png"> 
+			 <p>"Change" is a song by American rapper J. Cole, released on December 9, 2016 from his fourth studio album, 4 Your Eyez Only. J. Cole raps here about the changes he is going through as he's grows older and gains wisdom and maturity.</p>
+			 </center> </div>
+			 <div>Top 2 XXX by Kendrick Lamar<center><img src="xxx.png">
+			 <p>Kendrick teams up with rock legends U2 on “XXX.”, which delves deeper into the political and religious themes heard throughout DAMN(album name).</p>
+			 </center> </div>
+			 <div>Top 3 The Shade by Rex Orange County<center><img src="theshade.png">
+			 <p>The Shade released on March 11, 2022 in the album WHO CARES? made by Rex Orange County.</p> </center> </div>
+			 <div>Top 4 Face by BROCKHAMPTON<center><img src="face.jpg">
+			 <p>“FACE” is the first single off BROCKHAMPTON's debut album SATURATION and features various members of the boyband. </p> </center> </div>
+			 <div>Top 5 16 by Baby Keem<center><img src="16.jpg">
+			 <p>The final track of his debut album, 16 sees Baby Keem describing a tender yet crumbling relationship between himself and an unnamed woman with an unfortunate and impoverished upbringing. The track also happens to be the 16th song on the final album, matching its title.</p> </center> </div>
+			 <div>Top 6 Bound 2 by Kanye West<center><img src="bound2.jpg">
+			 <p>"Bound 2" is a song by American rapper Kanye West, featured as the final track from his sixth studio album, Yeezus (2013). It was produced by West and Che Pope, with additional production being handled by Eric Danchick, Noah Goldstein, No ID and Mike Dean.</p> </center> </div>
+			 <div>Top 7 Loving is Easy by Rex Orange County<center><img src="loving.jpg">
+			 <p>Loving Is Easy is a single by Rex Orange County that was released on October 11th, 2017, alongside a stop-motion style music video on Rex OC’s official YouTube channel.</p> </center> </div>
+			 <div>Top 8 the remedy for a broken heart by XXXTENTACION<center><img src="theremedy.jpg">
+			 <p>This track is likely about XXXTENTACION’s ex-girlfriend, Geneva Ayala, whom much of his debut album, 17, was about. He even named the outro on the album after her.</p> </center> </div>
+			 <div>Top 9 Faucet Failure by Ski Mask The Slump God<center><img src="faucetfailure.jpg">
+			 <p> “Faucet Failure” is an upbeat track from Ski Mask the Slump God, featuring him flexing his “drip,” or expensive clothing, comparing it to the way a faucet drips when it breaks. The track’s instrumental is centered around a steel drum and horn line with booming bass in the background.</p></center></div>
+			 <div>Top 10 Jocelyn Flores by XXXTENTACION<center><img src="jocelynflores.jpg">
+			 <p>Jocelyn Flores was a friend of X who suffered from depression and committed suicide in May 2017, while staying with him in Florida for a photoshoot.</p></center> </div>
+		</div>
+   </body>
+ </html>
