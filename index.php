48b1908ee0c24714548dca135d750b69fa4211e5
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-04-04 10:31:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<title>My Home Page</title>
+	<head><h1>Welcome To My Website! My Name is Jersey Zenith V. Setera</h1></head>
+	<a href="Recipes.html"><li>Learning Task 2</li></a>
+	<a href="Index.html"><li>Learning Task 3</li></a>
+	<a href="Overpopulation.html"><li>MIDTERM EXAM</li></a>
+	<a href="poem.html"><li>Learning Task 4</li></a>
+</html>
