f67afd3ff88cb498d8ca4d83baa373ab3bd93d32
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-04-04 10:38:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 	<title>My Home Page</title>
 	<head><h1>Welcome To My Website! My Name is Jersey Zenith V. Setera</h1></head>
-	<a href="Recipes.html"><li>Learning Task 2</li></a>
-	<a href="Index.html"><li>Learning Task 3</li></a>
-	<a href="Overpopulation.html"><li>MIDTERM EXAM</li></a>
+	<a href="LT2/Recipes.html"><li>Learning Task 2</li></a>
+	<a href="LT3/Index.html"><li>Learning Task 3</li></a>
+	<a href="CLE1MT/Overpopulation.html"><li>MIDTERM EXAM</li></a>
 	<a href="LT4/poem.html"><li>Learning Task 4</li></a>
 </html>
