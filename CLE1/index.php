3467e0681fb38870640e6c605bf7ddac291b5105
JerseyZenith <100018070+JerseyZenith@users.noreply.github.com>
2022-05-21 00:11:15 +0800

    Add files via upload

---
+++ CLE1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<title>CLE</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<?php
+echo "<nav class=\"navbar\">\n";
+echo "	 <ul class=\"link-group\">\n";
+echo "		<li class=\"link active\"><a href=\"index.php\">Home</a></li>\n";
+echo "		<li class=\"link\"><a href=\"Works.html\">Sample Works</a></li>\n";
+echo "		<li class=\"link\"><a href=\"Aboutme.html\">About Me</a></li>\n";
+echo "	 </ul>\n";
+echo "	</nav>";
+?>
+
+	
+
+ 
+<div class="home-section">
+    <h1 class="hero-heading">Welcome, to my Portfolio <br> My Name is Jersey</h1>
+    <img src="me.jpg" class="home-img" alt="">
+</div>
+
+<script src="java.js"></script>
+</html>
